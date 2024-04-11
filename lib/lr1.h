5e1efd113ef0a8fc323b4a2cf62b053b81310958
Daniil Lukashin <lukashin.danilka@mail.ru>
2024-04-11 14:58:44 +0400

    новые тесты

--- lib/lr1.h
+++ lib/lr1.h
@@ -4,4 +4,3 @@
 
 std::pair<int, std::vector<int>> replaceAndSum(const std::vector<int>& array);
 bool hasOddDigits(int num);
-std::vector<int> generateOddArray(int size);
--- tests/lr1_test.cpp
+++ tests/lr1_test.cpp
@@ -29,15 +29,6 @@ std::vector<int> generateOddArray(int size) {
     return array;
 }
 
-std::vector<int> generateEvenArray(int size) {
-    std::vector<int> array;
-    int num = 2;
-    for (int i = 0; i < size; ++i) {
-        array.push_back(num);
-        num += 2;
-    }
-    return array;
-}
 
 TEST_F(ReplaceAndSumTest, TestEmptyArray) {
     arr = {};
@@ -51,35 +42,6 @@ TEST_F(ReplaceAndSumTest, TestArraySize1) {
     EXPECT_EQ(replaceAndSum(arr), result[0]);
 }
 
-// Тест на массив из 53 элементов
-TEST_F(ReplaceAndSumTest, ArrayWith53Elements) {
-    arr = generateOddArray(53);
-    // Считаем сумму всех элементов массива
-    int sum = 0;
-
-    // Считаем сумму всех элементов массива
-    for (int num : arr) {
-        sum += num;
-    }
-
-    result = { {sum, {sum,sum,sum,sum,sum,sum,sum,sum,sum,sum, 21, 23, 25, 27, 29, sum,sum,sum,sum,sum,41, 43, 45, 47, 49, sum, sum, sum, sum, sum, 61, 63, 65, 67, 69, sum,sum,sum,sum,sum,81, 83, 85, 87, 89, sum, sum, sum, sum, sum, 101, 103, 105}} };
-    ASSERT_EQ(replaceAndSum(arr), result[0]);
-}
-
-// Тест на массив из 105 элементов
-TEST_F(ReplaceAndSumTest, ArrayWith105Elements) {
-    arr = generateEvenArray(105);
-    // Считаем сумму всех элементов массива
-    int sum = 0;
-
-    // Считаем сумму всех элементов массива
-    for (int num : arr) {
-        sum += num;
-    }
-
-    result = { {sum, arr} };
-    ASSERT_EQ(replaceAndSum(arr), result[0]);
-}
 
 
 
